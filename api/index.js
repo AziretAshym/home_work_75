95ff0f8ddd595883793e1294ff311d1a8f273866
Ashymov Aziret <aziretashym@gmail.com>
2024-12-05 15:52:35 +0600

    [API]: Connected "cors" library to be can make requests to another domain.
    [Client]: Made .post request to encrypt the transmitted word.

--- api/index.js
+++ api/index.js
@@ -1,26 +1,39 @@
-const express = require('express');
-const {Vigenere} = require('caesar-salad')
+import express from "express";
+import cors from "cors";
+import pkg from "caesar-salad";
+
+const { Vigenere } = pkg;
 
 const app = express();
-const port = 8000   ;
+const port = 8000;
 
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    return res.send('Home page');
+app.get("/", (req, res) => {
+    return res.send("Home page");
 });
 
-app.post('/encode/', (req, res) => {
-    const {password, message} = req.body;
-    return res.json(Vigenere.Cipher(password).crypt(message));
+app.post("/encode", (req, res) => {
+    const { password, message } = req.body;
+
+    try {
+        return res.json({ encoded: Vigenere.Cipher(password).crypt(message) });
+    } catch (e) {
+        console.error(e);
+    }
 });
 
-app.post('/decode/', (req, res) => {
-    const {password, message} = req.body;
-    return res.json(Vigenere.Decipher(password).crypt(message));
-});
+app.post("/decode", (req, res) => {
+    const { password, message } = req.body;
 
+    try {
+        return res.json({ decoded: Vigenere.Decipher(password).crypt(message) });
+    } catch (e) {
+        console.error(e);
+    }
+});
 
 app.listen(port, () => {
-    console.log(`server started on port: http://localhost:${port}`);
-})
-
+    console.log(`Server started on http://localhost:${port}`);
+});
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,9 +2,36 @@ import Navbar from './components/Navbar/Navbar.tsx';
 import { Button, Container, CssBaseline, FormControl, Input, InputLabel, OutlinedInput } from '@mui/material';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import axiosApi from './axiosApi.ts';
+import { useState } from 'react';
 
 const App = () => {
 
+  const [encodeInput, setEncodeInput] = useState("");
+  const [encodedMessage, setEncodedMessage] = useState("");
+
+  const [passwordInput, setPasswordInput] = useState("");
+
+
+  const password = "hello";
+
+  const getEncodedMessage = async () => {
+    try {
+      if (encodeInput && passwordInput === "hello") {
+        const response = await axiosApi.post("/encode", {password: password, message: encodeInput});
+        setEncodeInput("");
+        setEncodedMessage(response.data.encoded);
+        console.log(response.data.encoded);
+        setPasswordInput("");
+      } else {
+        alert("Word for encode not field or incorrect password!");
+      }
+
+
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <>
@@ -16,25 +43,42 @@ const App = () => {
         <Container component="main" sx={{flexGrow: 1, display: "flex", gap: "40px", justifyContent: "space-between", mb: "40px"}}>
           <FormControl sx={{width: "300px"}}>
             <InputLabel htmlFor="my-encode_input">Encode word</InputLabel>
-            <OutlinedInput id="encode_input"/>
+            <OutlinedInput
+              id="encode_input"
+              value={encodeInput}
+              onChange={(e) => setEncodeInput(e.target.value)}
+            />
           </FormControl>
 
           <FormControl sx={{width: "300px"}}>
             <InputLabel htmlFor="password_input">Password</InputLabel>
-            <Input id="password_input"/>
+            <Input
+              id="password_input"
+              value={passwordInput}
+              onChange={(e) => setPasswordInput(e.target.value)}
+            />
           </FormControl>
 
           <FormControl sx={{width: "300px"}}>
             <InputLabel htmlFor="decode_input">Decode word</InputLabel>
-            <OutlinedInput id="decode_input"/>
+            <OutlinedInput
+              id="decode_input"
+              value={encodedMessage}
+            />
           </FormControl>
         </Container>
         <div style={{display: "flex", justifyContent: "center"}}>
-          <Button type={"button"}>
+          <Button
+            type={"button"}
+          >
             <ArrowBackIcon />
           </Button>
-          <Button type={"button"}>
-            <ArrowForwardIcon />
+          <Button
+            type={"button"}
+            onClick={getEncodedMessage}
+            variant="contained"
+          >
+              <ArrowForwardIcon />
           </Button>
         </div>
       </main>
